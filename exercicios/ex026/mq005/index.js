e544b4319ef8e07a89b6a257eea48986d056b240
Marco Antônio <marcoantoniocamposcarrocon@gmail.com>
2023-09-08 13:59:55 -0300

    menu responsivo

--- exercicios/ex026/mq005/index.js
+++ exercicios/ex026/mq005/index.js
@@ -1,4 +1,12 @@
+const body = document.body;
 const btn = document.querySelector(".fa-bars");
 const menu = document.querySelector("menu");
 
 btn.addEventListener('click', () => menu.classList.toggle('open'));
+body.addEventListener('resize', () => {
+    if (window.innerWidth >= 812) {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+});
